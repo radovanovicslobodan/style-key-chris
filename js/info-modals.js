e355ca836f708cc0c8ad065c923a7f49913e3d08
Boban <bobandevelop@gmail.com>
2015-04-16 22:48:46 +0200

    majority of measurements-men.html finished

---
+++ js/info-modals.js
@@ -0,0 +1,137 @@
+$(function () {
+  $('.weight-m').on('click', function ( e ) {
+    $.fn.custombox( this, {
+      effect: 'fadein',
+      position: 'center',
+      overlayColor: '#fff',
+      overlayOpacity: 0.5
+    });
+    e.preventDefault();
+  });
+  $('.neck-m').on('click', function ( e ) {
+    $.fn.custombox( this, {
+      effect: 'fadein',
+      position: 'center',
+      overlayColor: '#fff',
+      overlayOpacity: 0.5
+    });
+    e.preventDefault();
+  });
+  $('.shoulder-m').on('click', function ( e ) {
+    $.fn.custombox( this, {
+      effect: 'fadein',
+      position: 'center',
+      overlayColor: '#fff',
+      overlayOpacity: 0.5
+    });
+    e.preventDefault();
+  });
+  $('.chest-m').on('click', function ( e ) {
+    $.fn.custombox( this, {
+      effect: 'fadein',
+      position: 'center',
+      overlayColor: '#fff',
+      overlayOpacity: 0.5
+    });
+    e.preventDefault();
+  });
+  $('.sleevelength-m').on('click', function ( e ) {
+    $.fn.custombox( this, {
+      effect: 'fadein',
+      position: 'center',
+      overlayColor: '#fff',
+      overlayOpacity: 0.5
+    });
+    e.preventDefault();
+  });
+  $('.waist-m').on('click', function ( e ) {
+    $.fn.custombox( this, {
+      effect: 'fadein',
+      position: 'center',
+      overlayColor: '#fff',
+      overlayOpacity: 0.5
+    });
+    e.preventDefault();
+  });
+  $('.hips-m').on('click', function ( e ) {
+    $.fn.custombox( this, {
+      effect: 'fadein',
+      position: 'center',
+      overlayColor: '#fff',
+      overlayOpacity: 0.5
+    });
+    e.preventDefault();
+  });
+  $('.outseam-m').on('click', function ( e ) {
+    $.fn.custombox( this, {
+      effect: 'fadein',
+      position: 'center',
+      overlayColor: '#fff',
+      overlayOpacity: 0.5
+    });
+    e.preventDefault();
+  });
+  $('.inseam-m').on('click', function ( e ) {
+    $.fn.custombox( this, {
+      effect: 'fadein',
+      position: 'center',
+      overlayColor: '#fff',
+      overlayOpacity: 0.5
+    });
+    e.preventDefault();
+  });
+  $('.jacketlength-m').on('click', function ( e ) {
+    $.fn.custombox( this, {
+      effect: 'fadein',
+      position: 'center',
+      overlayColor: '#fff',
+      overlayOpacity: 0.5
+    });
+    e.preventDefault();
+  });
+  $('.bicep-m').on('click', function ( e ) {
+    $.fn.custombox( this, {
+      effect: 'fadein',
+      position: 'center',
+      overlayColor: '#fff',
+      overlayOpacity: 0.5
+    });
+    e.preventDefault();
+  });
+  $('.wrist-m').on('click', function ( e ) {
+    $.fn.custombox( this, {
+      effect: 'fadein',
+      position: 'center',
+      overlayColor: '#fff',
+      overlayOpacity: 0.5
+    });
+    e.preventDefault();
+  });
+  $('.rise-m').on('click', function ( e ) {
+    $.fn.custombox( this, {
+      effect: 'fadein',
+      position: 'center',
+      overlayColor: '#fff',
+      overlayOpacity: 0.5
+    });
+    e.preventDefault();
+  });
+  $('.thigh-m').on('click', function ( e ) {
+    $.fn.custombox( this, {
+      effect: 'fadein',
+      position: 'center',
+      overlayColor: '#fff',
+      overlayOpacity: 0.5
+    });
+    e.preventDefault();
+  });
+  $('.knee-m').on('click', function ( e ) {
+    $.fn.custombox( this, {
+      effect: 'fadein',
+      position: 'center',
+      overlayColor: '#fff',
+      overlayOpacity: 0.5
+    });
+    e.preventDefault();
+  });
+});
