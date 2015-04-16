9a0b25ae7921e8fbf156f65b03ad6c1d30f58455
Boban <bobandevelop@gmail.com>
2015-04-17 01:22:35 +0200

    even more optimized js code for modals

--- js/info-modals.js
+++ js/info-modals.js
@@ -5,86 +5,16 @@ $(function () {
     overlayColor :'#fff',
     overlayOpacity :'0.5'
   };
+
   // men
-  $('.weight-m').on('click', function ( e ) {
+
+  var $modItems = $('.weight-m, .neck-m, .shoulder-m, .chest-m, .sleevelength-m, .waist-m, .hips-m, .outseam-m, .inseam-m, .footlength-m, .jacketlength-m, .bicep-m, .wrist-m, .waistcoat-m, .pantswaistband-m, .rise-m, .thigh-m, .knee-m, .headsize-m, .glovesize-m');
+
+  $modItems.on('click', function ( e ) {
     $.fn.custombox( this, settings);
     e.preventDefault();
   });
-  $('.neck-m').on('click', function ( e ) {
-    $.fn.custombox( this, settings);
-    e.preventDefault();
-  });
-  $('.shoulder-m').on('click', function ( e ) {
-    $.fn.custombox( this, settings);
-    e.preventDefault();
-  });
-  $('.chest-m').on('click', function ( e ) {
-    $.fn.custombox( this, settings);
-    e.preventDefault();
-  });
-  $('.sleevelength-m').on('click', function ( e ) {
-    $.fn.custombox( this, settings);
-    e.preventDefault();
-  });
-  $('.waist-m').on('click', function ( e ) {
-    $.fn.custombox( this, settings);
-    e.preventDefault();
-  });
-  $('.hips-m').on('click', function ( e ) {
-    $.fn.custombox( this, settings);
-    e.preventDefault();
-  });
-  $('.outseam-m').on('click', function ( e ) {
-    $.fn.custombox( this, settings);
-    e.preventDefault();
-  });
-  $('.inseam-m').on('click', function ( e ) {
-    $.fn.custombox( this, settings);
-    e.preventDefault();
-  });
-  $('.footlength-m').on('click', function ( e ) {
-    $.fn.custombox( this, settings);
-    e.preventDefault();
-  });
-  $('.jacketlength-m').on('click', function ( e ) {
-    $.fn.custombox( this, settings);
-    e.preventDefault();
-  });
-  $('.bicep-m').on('click', function ( e ) {
-    $.fn.custombox( this, settings);
-    e.preventDefault();
-  });
-  $('.wrist-m').on('click', function ( e ) {
-    $.fn.custombox( this, settings);
-    e.preventDefault();
-  });
-  $('.waistcoat-m').on('click', function ( e ) {
-    $.fn.custombox( this, settings);
-    e.preventDefault();
-  });
-  $('.pantswaistband-m').on('click', function ( e ) {
-    $.fn.custombox( this, settings);
-    e.preventDefault();
-  });
-  $('.rise-m').on('click', function ( e ) {
-    $.fn.custombox( this, settings);
-    e.preventDefault();
-  });
-  $('.thigh-m').on('click', function ( e ) {
-    $.fn.custombox( this, settings);
-    e.preventDefault();
-  });
-  $('.knee-m').on('click', function ( e ) {
-    $.fn.custombox( this, settings);
-    e.preventDefault();
-  });
-  $('.headsize-m').on('click', function ( e ) {
-    $.fn.custombox( this, settings);
-    e.preventDefault();
-  });
-  $('.glovesize-m').on('click', function ( e ) {
-    $.fn.custombox( this, settings);
-    e.preventDefault();
-  });
+
   // women
+
 });
