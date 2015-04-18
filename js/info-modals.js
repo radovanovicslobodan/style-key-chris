83a7a4c61a878614e15520b063999d035dd23d3f
Boban <bobandevelop@gmail.com>
2015-04-18 23:38:54 +0200

    added some html,css and js comments

--- js/info-modals.js
+++ js/info-modals.js
@@ -1,28 +1,29 @@
-$(function () {
+$(function() {
   var modalSettings = {
-    effect : 'fadein',
-    // position :'center, center',
-    overlayColor :'#fff',
-    overlayOpacity :'0.5'
+    effect: "fadein",
+    overlayColor: "#fff",
+    overlayOpacity: "0.5"
   };
 
-  // men
+  // man
 
-  var $modItemsMan = $('.height-m, .chest-m, .waist-m, .upperbody-m, .waisband-m, .hips-m, .shoulder-m, .sleevelength-m, .outseam-m, .inseam-m, .neck-m, .jacketlength-m, .bicep-m, .wrist-m, .rise-m, .thigh-m, .knee-m');
+  var $modItemsMan = $(
+    ".height-m, .chest-m, .waist-m, .upperbody-m, .waisband-m, .hips-m, .shoulder-m, .sleevelength-m, .outseam-m, .inseam-m, .neck-m, .jacketlength-m, .bicep-m, .wrist-m, .rise-m, .thigh-m, .knee-m"
+  );
 
-  $modItemsMan.on('click', function ( e ) {
-    $.fn.custombox( this, modalSettings);
+  $modItemsMan.on("click", function(e) {
+    $.fn.custombox(this, modalSettings);
     e.preventDefault();
   });
 
-  // women
+  // woman
 
-  var $modItemsWoman = $('.height-w, .bust-w, .waist-w, .hips-w, .shoulder-w, .sleevelength-w, .waistpoint-w, .outseam-w, .inseam-w, .neck-w, .underbust-w, .bicep-w, .wrist-w, .bustpoint-w, .rise-w, .thigh-w, .knee-w');
+  var $modItemsWoman = $(
+    ".height-w, .bust-w, .waist-w, .hips-w, .shoulder-w, .sleevelength-w, .waistpoint-w, .outseam-w, .inseam-w, .neck-w, .underbust-w, .bicep-w, .wrist-w, .bustpoint-w, .rise-w, .thigh-w, .knee-w"
+  );
 
-   $modItemsWoman.on('click', function ( e ) {
-    $.fn.custombox( this, modalSettings);
+  $modItemsWoman.on("click", function(e) {
+    $.fn.custombox(this, modalSettings);
     e.preventDefault();
   });
-
-
 });
