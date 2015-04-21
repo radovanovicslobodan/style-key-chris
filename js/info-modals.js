4e140108f00b4c11f02f22901d9170218288f97e
Boban <bobandevelop@gmail.com>
2015-04-21 20:15:12 +0200

    final js improvement

--- js/info-modals.js
+++ js/info-modals.js
@@ -5,25 +5,10 @@ $(function() {
     overlayOpacity: "0.5"
   };
 
-  // man
+  var $infoModalsItems = $('a.info-btn');
 
-  var $modItemsMan = $(
-    ".height-m, .chest-m, .waist-m, .upperbody-m, .waisband-m, .hips-m, .shoulder-m, .sleevelength-m, .outseam-m, .inseam-m, .neck-m, .jacketlength-m, .bicep-m, .wrist-m, .rise-m, .thigh-m, .knee-m"
-  );
-
-  $modItemsMan.on("click", function(e) {
-    $.fn.custombox(this, modalSettings);
-    e.preventDefault();
-  });
-
-  // woman
-
-  var $modItemsWoman = $(
-    ".height-w, .bust-w, .waist-w, .hips-w, .shoulder-w, .sleevelength-w, .waistpoint-w, .outseam-w, .inseam-w, .neck-w, .underbust-w, .bicep-w, .wrist-w, .bustpoint-w, .rise-w, .thigh-w, .knee-w"
-  );
-
-  $modItemsWoman.on("click", function(e) {
-    $.fn.custombox(this, modalSettings);
+  $infoModalsItems.on('click', function ( e ) {
+    $.fn.custombox( this, modalSettings);
     e.preventDefault();
   });
 });
